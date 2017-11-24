829a59889abbcc4c72d104f3d78ee03d25fd4c74
Seres Gyozo <osereso@gmail.com>
2017-11-24 13:55:19 +0100

    Simplifying lines where strike (X,x) is used to .lower()

--- bowling.py
+++ bowling.py
@@ -3,29 +3,29 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+        if game[i] == '/': # tehát spare van, de azt még nem lehet 
+            result += 10 - last # what is this "last"?
         else:
             result += get_value(game[i])
-        # if not in_first_half:
+        # if not in_first_half: ## erre a két komment sorra mi szükség van?
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+        if frame < 10 and get_value(game[i]) == 10: # tehát a spare-t vagy strike-ot vizsgálja
+            if game[i] == '/': # ha spare van - ilyen már volt, ezt nem lehet def-be kiszedni?
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == "x":
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(game[i])
+        last = get_value(game[i]) # ennek nem itt kellene lennie, de akkor hol?
         if not in_first_half:
             frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].lower() == "x":
             in_first_half = True
             frame += 1
     return result
@@ -35,9 +35,7 @@ def get_value(char):
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
