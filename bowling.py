d870862af29405c7a33581e785d9a743ba428824
Seres Gyozo <osereso@gmail.com>
2017-11-24 18:32:32 +0100

    Magic numbers removed by adding two variables: max score (global) and total frames (local).

--- bowling.py
+++ bowling.py
@@ -1,11 +1,14 @@
+max_score = 10
+
 def score(game):
+    global max_score
     result = 0
     frame = 1
     last = 0
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last 
+            result += max_score - last 
         else:
             result += get_value(game[i])
         result = calculate_result(frame, game, i, result)
@@ -19,22 +22,25 @@ def score(game):
     return result
 
 def calculate_result(frame, game, i, result):
-    if frame < 10 and get_value(game[i]) == 10:
+    global max_score
+    total_frames = 10
+    if frame < total_frames and get_value(game[i]) == max_score:
         result += get_value(game[i+1])    
         if game[i].lower() == "x":
             if game[i+2] == '/':
-                result += 10 - get_value(game[i+1])
+                result += max_score - get_value(game[i+1])
             else:
                 result += get_value(game[i+2])
     return result
 
 def get_value(char): 
+    global max_score
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9': # range
         return int(char)
     elif char.lower() == 'x' or char == '/':
-        return 10
+        return max_score
     elif char == '-':
         return 0
     else:
