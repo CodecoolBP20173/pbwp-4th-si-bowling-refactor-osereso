2152ea1ceea3886dee0764714e993c1adadaf257
Seres Gyozo <osereso@gmail.com>
2017-11-26 11:59:09 +0100

    In get_value recognizing character is simplified.

--- bowling.py
+++ bowling.py
@@ -35,9 +35,7 @@ def calculate_result(frame, game, i, result):
 
 def get_value(char): 
     global max_score
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9': # range
+    if char in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
         return int(char)
     elif char.lower() == 'x' or char == '/':
         return max_score
